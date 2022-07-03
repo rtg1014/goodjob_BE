7ae25da2b04e912834f08b8afb0c604347b043aa
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-03 23:58:12 +0900

    ADD: controller 형식 예시

---
+++ controller/authController.js
@@ -0,0 +1,18 @@
+const { regex, asyncWrapper } = require('../utils/util');
+
+module.exports = {
+  create: {
+    local: asyncWrapper(async (req, res) => {
+      const { email, password, confirmPassword, userName } = req.body;
+      if (!regex.checkEmail(email)) {
+        return res.status(400).json({
+          isSuccess: false,
+          msg: '이메일 형식이 올바르지 않습니다.',
+        });
+      }
+    }),
+  },
+  update: {},
+  get: {},
+  delete: {},
+};
