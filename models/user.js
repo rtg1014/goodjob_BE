137f0487cade18b671471d49c00c134053eb70c0
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-04 02:11:13 +0900

    FIX: 오타수정

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      username: {
+      userName: {
         type: DataTypes.STRING,
         allowNull: false,
       },
