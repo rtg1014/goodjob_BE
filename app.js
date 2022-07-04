5677e10e3b6a5b386228a2aa9949e1cbd25c3417
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-05 03:00:50 +0900

    FIX: credentials 옵션 수정

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
   app.use(
     cors({
-      origin: '*',
-      // credentials: true,
+      origin: 'http://localhost:3000',
+      credentials: true,
     })
   );
 }
