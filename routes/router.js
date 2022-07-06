d20a75c8f4f067ef2ab92f9ec3484f3e3b837828
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-06 21:07:45 +0900

    ADD: 비밀번호 찾기 기능

--- routes/router.js
+++ routes/router.js
@@ -10,10 +10,12 @@ const middleware = require('../utils/middleware');
 
 // api/auth
 router.post('/auth/local', AuthController.create.local); // 이메일 인증받고 가입하기
-router.post('/auth/verifyNumber', AuthController.create.verifyNumber); // 인증 번호 확인 and 회원가입
-router.get('/auth/kakao/callback', AuthController.create.kakao); // 카카오 로그인 콜백
+router.post('/auth/verifyNumberForNew', AuthController.create.verifyNumberForNew); // 인증 번호 확인 and 회원가입
+router.patch('/auth/lostPassword', AuthController.update.lostPassword); // 인증 메일 발송(비밀번호 분실)
+router.patch('/auth/verifyNumberForOld', AuthController.create.verifyNumberForOld); // 인증 번호 확인(비밀번호 분실)
 router.patch('/auth/newPassword', AuthController.update.newPassword); //비밀번호 재설정
 router.post('/auth', AuthController.get.auth); // 로그인
+router.get('/auth/kakao/callback', AuthController.create.kakao); // 카카오 로그인 콜백
 // router.delete("/auth/logout", middleware.auth, AuthController.delete.auth);   // 로그아웃
 // router.get("/user/me", middleware.auth, UserController.get.user);   // 로그인 유저 정보 가져오기
 
