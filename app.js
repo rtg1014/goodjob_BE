bb393a337b99c6268c298bcd8fe4c9a5490ef7d7
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-07 19:48:22 +0900

    FIX: 개발환경 cors 수정

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: '*',
       credentials: true,
     })
   );
