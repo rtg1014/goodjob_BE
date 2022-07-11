52fc661e732991581ec352de149f3c4c654d2616
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-12 01:34:03 +0900

    ADD: seeders 50%

---
+++ seeders/initJob.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert("jobs", [
+    //   {
+    //     type: "신입",
+    //   }, 
+
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
