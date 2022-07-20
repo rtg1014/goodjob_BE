febf5a5146a107ca785bea2a5e81b8aa3a4e02ef
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-21 04:27:29 +0900

    ADD: test for verifying auth number 50%

--- test/unit/user.test.js
+++ test/unit/user.test.js
@@ -6,8 +6,9 @@ const locals = require('../data/locals.json');
 const signup1 = require('../data/signup1.json');
 const signup2 = require('../data/signup2.json');
 const signup3 = require('../data/signup3.json');
-const {auth, joiMiddleware} = require('../../utils/middleware');
-
+let signup4 = require('../data/signup4.json');
+const signup5 = require('../data/signup5.json');
+// const {auth, joiMiddleware} = require('../../utils/middleware');
 
 User.findOne = jest.fn();
 User.create = jest.fn();
@@ -22,10 +23,10 @@ beforeEach(() => {
   next = null;
   res.locals.user = locals;
 });
+
 describe('회원가입', () => {
   test('회원가입 가능할 때', async () => {
     req.body = signup1;
-    joiMiddleware('localSchema')
     AuthEmail.create.mockResolvedValue(signup2);
     await AuthController.create.local(req, res, next);
     expect(res._getJSONData()).toStrictEqual({
@@ -33,13 +34,35 @@ describe('회원가입', () => {
       msg: '메일 발송 완료. 메일함을 확인해 주세요.',
     });
   });
-//   test('password와 confirmPassword 다른 경우', async () => {
-//       req.body = signup3;
-//       joiMiddleware('localSchema')(req,res,next)
-//       await AuthController.create.local(req, res, next);
-//       expect(res._getJSONData()).toStrictEqual({
-//           fail: '비밀번호가 일치하지 않습니다.',
-//       });
-//   });
 
+  test('아이디 중복 에러', async () => {
+    req.body = signup3;
+    User.findOne.mockResolvedValue(signup3);
+    await AuthController.create.local(req, res, next);
+    expect(res._getJSONData()).toStrictEqual({
+      isSuccess: false,
+      msg: '이미 존재하는 이메일입니다.',
+    });
+  });
+});
+
+describe('회원가입(메일 인증)', () => {
+  // test('메일 인증, 회원 가입 완료', async () => {
+  //   req.body = signup4;
+  //   await AuthController.create.verifyNumberForNew(req, res, next);
+  //   expect(res._getJSONData()).toStrictEqual({
+  //     isSuccess: true,
+  //     msg: '인증이 완료되었습니다. 새로운 비밀번호를 입력해주세요.',
+  //   });
+  // });
+
+  test('메일 인증, 인증 번호 오류', async () => {
+    req.body = signup5;
+    AuthEmail.findOne.mockResolvedValue(signup5);
+    await AuthController.create.verifyNumberForNew(req, res, next);
+    expect(res._getJSONData()).toStrictEqual({
+      isSuccess: false,
+      msg: '인증 번호가 틀렸습니다.',
+    });
+  });
 });
