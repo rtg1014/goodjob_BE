df835f85b9cec49f74af306181221322e4cdd7a1
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-20 23:09:01 +0900

    ADD: temp save

--- test/unit/user.test.js
+++ test/unit/user.test.js
@@ -5,6 +5,9 @@ const AuthEmail = require('../../models/authEmail');
 const locals = require('../data/locals.json');
 const signup1 = require('../data/signup1.json');
 const signup2 = require('../data/signup2.json');
+const signup3 = require('../data/signup3.json');
+const {auth, joiMiddleware} = require('../../utils/middleware');
+
 
 User.findOne = jest.fn();
 User.create = jest.fn();
@@ -19,10 +22,10 @@ beforeEach(() => {
   next = null;
   res.locals.user = locals;
 });
-
 describe('회원가입', () => {
   test('회원가입 가능할 때', async () => {
     req.body = signup1;
+    joiMiddleware('localSchema')
     AuthEmail.create.mockResolvedValue(signup2);
     await AuthController.create.local(req, res, next);
     expect(res._getJSONData()).toStrictEqual({
@@ -30,4 +33,13 @@ describe('회원가입', () => {
       msg: '메일 발송 완료. 메일함을 확인해 주세요.',
     });
   });
+//   test('password와 confirmPassword 다른 경우', async () => {
+//       req.body = signup3;
+//       joiMiddleware('localSchema')(req,res,next)
+//       await AuthController.create.local(req, res, next);
+//       expect(res._getJSONData()).toStrictEqual({
+//           fail: '비밀번호가 일치하지 않습니다.',
+//       });
+//   });
+
 });
