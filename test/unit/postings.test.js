277826ff8639dee0b3e1ce3818922132add4170a
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-22 19:07:07 +0900

    ADD: posting controller (30%) // test

--- test/unit/postings.test.js
+++ test/unit/postings.test.js
@@ -4,6 +4,14 @@ const { User, AuthEmail, User_info } = require('../../models');
 const user = require('../data/posting/user.json');
 const category1 = require('../data/posting/category1.json');
 const category2 = require('../data/posting/category2.json');
+const category3 = require('../data/posting/category3.json');
+const category4 = require('../data/posting/category4.json');
+const category5 = require('../data/posting/category5.json');
+const category6 = require('../data/posting/category6.json');
+const category7 = require('../data/posting/category7.json');
+const category8 = require('../data/posting/category8.json');
+const category9 = require('../data/posting/category9.json');
+const category10 = require('../data/posting/category10.json');
 
 // jest.fn()
 User.findOne = jest.fn();
@@ -14,32 +22,81 @@ AuthEmail.create = jest.fn();
 AuthEmail.updateOne = jest.fn();
 AuthEmail.destroy = jest.fn();
 User_info.findOne = jest.fn();
-sendMailMock = jest.fn()
+User_info.updateOne = jest.fn();
+sendMailMock = jest.fn();
 
 beforeEach(() => {
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
-    next = null;
-    req.user = user
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+  req.user = user;
 });
 
 describe('카테고리 조회', () => {
-    test('조회 성공', async () => {
-        User_info.findOne.mockResolvedValue(category1)
-        await PostingController.get.category(req, res, next);
-        expect(res._getJSONData()).toStrictEqual({
-            isSuccess: true,
-            data: category2,
-            msg: '카테고리 조회 완료!',
-        });
+  test('조회 성공', async () => {
+    User_info.findOne.mockResolvedValue(category1);
+    await PostingController.get.category(req, res, next);
+    expect(res._getJSONData()).toStrictEqual({
+      isSuccess: true,
+      data: category2,
+      msg: '카테고리 조회 완료!',
     });
+  });
 
-    test('조회 실패 에러', async () => {
-        User_info.findOne.mockResolvedValue(undefined)
-        await PostingController.get.category(req, res, next);
-        expect(res._getJSONData()).toStrictEqual({
-            isSuccess: false,
-            msg: '카테고리 조회 실패',
-        });
+  test('조회 실패 에러', async () => {
+    User_info.findOne.mockResolvedValue(undefined);
+    await PostingController.get.category(req, res, next);
+    expect(res._getJSONData()).toStrictEqual({
+      isSuccess: false,
+      msg: '카테고리 조회 실패',
     });
+  });
+});
+
+describe('카테고리 변경', () => {
+  test('변경 성공(경력)', async () => {
+    req.body = category3;
+    User_info.updateOne.mockResolvedValue(category4);
+    await PostingController.update.category(req, res, next);
+    expect(res._getJSONData()).toStrictEqual({
+      isSuccess: true,
+      msg: '카테고리 변경 완료',
+    });
+  });
+
+  test('변경 성공(기업 형태)', async () => {
+    req.body = category5;
+    User_info.updateOne.mockResolvedValue(category6);
+    await PostingController.update.category(req, res, next);
+    expect(res._getJSONData()).toStrictEqual({
+      isSuccess: true,
+      msg: '카테고리 변경 완료',
+    });
+  });
+
+  test('변경 성공(근무지)', async () => {
+    req.body = category7;
+    User_info.updateOne.mockResolvedValue(category8);
+    await PostingController.update.category(req, res, next);
+    expect(res._getJSONData()).toStrictEqual({
+      isSuccess: true,
+      msg: '카테고리 변경 완료',
+    });
+  });
+
+  test('변경 성공(직무)', async () => {
+    req.body = category9;
+    User_info.updateOne.mockResolvedValue(category10);
+    await PostingController.update.category(req, res, next);
+    expect(res._getJSONData()).toStrictEqual({
+      isSuccess: true,
+      msg: '카테고리 변경 완료',
+    });
+  });
+
+  test('변경 실패 에러', async () => {
+    User_info.updateOne.mockResolvedValue(undefined);
+    await PostingController.update.category(req, res, next);
+    expect(res.statusCode).toBe(500);
+  });
 });
