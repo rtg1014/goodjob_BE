0acbc61e6120223569647330aa09a4c33b4f611e
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-23 18:18:18 +0900

    ADD: 개인 스케줄 작성

--- test/unit/schedule.test.js
+++ test/unit/schedule.test.js
@@ -4,7 +4,10 @@ const {
   user_schedule,
   Schedule,
 } = require('../../models');
-const user = require('../data/posting/user.json');
+const user = require('../data/schedule/user.json');
+const mySchedule1 = require('../data/schedule/mySchedule1.json');
+const mySchedule2 = require('../data/schedule/mySchedule2.json');
+const mySchedule3 = require('../data/schedule/mySchedule3.json');
 
 // jest.fn()
 Schedule.create = jest.fn();
@@ -18,26 +21,14 @@ beforeEach(() => {
 });
 
 describe('스케줄 생성(작성)', () => {
-//   test('생성 성공', async () => {
-//     User_info.findOne.mockResolvedValue(category1);
-//     await ScheduleController.create.mySchedule(req, res, next);
-//     expect(res._getJSONData()).toStrictEqual({
-//         isSuccess: true,
-//         msg: '개인 스케줄 작성이 완료되었습니다.',
-//     });
-//   });
-
-//   test('조회 실패 에러', async () => {
-//     User_info.findOne.mockResolvedValue(undefined);
-//     await PostingController.get.category(req, res, next);
-//     expect(res._getJSONData()).toStrictEqual({
-//       isSuccess: false,
-//       msg: '카테고리 조회 실패',
-//     });
-//   });
-
-  test("Just pass", () => {
-    expect(() => {
-    }).not.toThrow();
+  test('생성 성공', async () => {
+    req.body = mySchedule1
+    Schedule.create.mockResolvedValue(mySchedule2);
+    user_schedule.create.mockResolvedValue(mySchedule3);
+    await ScheduleController.create.mySchedule(req, res, next);
+    expect(res._getJSONData()).toStrictEqual({
+        isSuccess: true,
+        msg: '개인 스케줄 작성이 완료되었습니다.',
+    });
   });
 });
