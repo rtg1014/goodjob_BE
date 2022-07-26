ffb20a7afe0b145685761125a803fa6bbc819e89
seongwon <87584033+Magiof@users.noreply.github.com>
2022-07-27 04:26:04 +0900

    FIX: gitignore test

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const app = require('./app');
 const fs = require('fs');
-const https = require('https');
-// const server = require("http").createServer(app);
+// const https = require('https');
+const server = require("http").createServer(app);
 const dotenv = require('dotenv');
 dotenv.config();
 
-const certificate = fs.readFileSync(
-  '/etc/letsencrypt/live/goodjobcalendar.com/fullchain.pem',
-  'utf8'
-);
-const privateKey = fs.readFileSync(
-  '/etc/letsencrypt/live/goodjobcalendar.com/privkey.pem',
-  'utf8'
-);
+// const certificate = fs.readFileSync(
+//   '/etc/letsencrypt/live/goodjobcalendar.com/fullchain.pem',
+//   'utf8'
+// );
+// const privateKey = fs.readFileSync(
+//   '/etc/letsencrypt/live/goodjobcalendar.com/privkey.pem',
+//   'utf8'
+// );
 
-const credentials = {
-  cert: certificate,
-  key: privateKey,
-};
+// const credentials = {
+//   cert: certificate,
+//   key: privateKey,
+// };
 
-const server = https.createServer(credentials, app);
+// const server = https.createServer(credentials, app);
 
 
 server.listen(process.env.PORT, () => {
