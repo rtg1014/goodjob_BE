286812552749b18f6599f1e202e132a0464f61d2
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2022-07-29 02:05:17 +0900

    FIX : 깃 초기화

--- validators/scrap.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const scrapSchema = Joi.object({
-  postingId: Joi.number().integer().empty().required().messages({
-    'string.empty': 'postingId 값을 입력해 주세요.',
-  }),
-});
-
-module.exports = scrapSchema;
