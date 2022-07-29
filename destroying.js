6358e189db4cb0bf8626bad7b2fe6a77951c311a
magiof <tjddnjs7559@gmail.com>
2022-07-30 04:53:57 +0900

    FIX: 만료 데이터 삭제 정책 변경

--- destroying.js
+++ destroying.js
@@ -5,31 +5,12 @@ const { Op } = require('sequelize');
 
 async function destroying() {
     let now = dateFormatter(new Date())
-
-    const t = await sequelize.transaction();
-
     try {
-        await Schedule.destroy({
-            where: {
-                date: { [Op.lte]: now },
-                postingId: { [Op.ne]: null }
-            }
-        }, { transaction: t })
-
-        await Posting.destroy({
-            where: {
-                deadline: { [Op.lte]: now }
-            }
-        }, { transaction: t })
-
-        await t.commit();
+        await Posting.destroy({ where: { deadline: { [Op.lte]: now } } })
         console.log('데이터 삭제 완료!');
     } catch (error) {
-        await t.rollback();
         console.log('데이터 삭제 실패!');
     }
 }
-
-destroying()
 
 module.exports = destroying
